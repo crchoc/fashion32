7f84ec1fa6d499296a1b4a51144742de68a086c0
crchoc <crchoc@konkuk.ac.kr>
2023-08-09 13:57:02 +0900

    project parameters

---
+++ resource/parameter.py
@@ -0,0 +1,28 @@
+IMG_DIR = '../../dataset' # path to image data
+BAD_IMGS = '../jsons/bad_images.json' # bad images
+
+LABELS_FILE = '../jsons/labels_type.json' # labels type info
+PATTERNS_FILE = '../jsons/pattern_type.json' # pattern type info
+MATERIAL_FILE = '../jsons/material_type.json' # material type info
+SLEEVE_TYPE_FILE = '../jsons/sleeve_type.json' # sleeve type info
+CATEGORY_FILE = '../jsons/category_type.json' # category type info
+FIT_FILE = '../jsons/fit.json' # fit type info
+GENDER_FILE = '../jsons/gender.json' # gender info
+OCCASION_FILE = '../jsons/occasion.json' # occasion info
+STYLE_FILE = '../jsons/style.json' # style info
+IMGS_LABEL = '../jsons/imgs_labels.json' # labels of images
+
+ALL_DATA_FILE = '../jsons/all_data.json' # all data
+REARRANGED_DATA = '../jsons/rearranged_data.json' # rearranged info
+
+TRAIN_DATA_FILE = '../jsons/train_data.json' # train data
+TEST_DATA_FILE = '../jsons/test_data.json' # test data
+VAL_DATA_FILE = '../jsons/val_data.json' # val data
+
+TRAIN_FEATS = '../jsons/train_features.pkl' # train features
+TEST_FEATS = '../jsons/test_features.pkl' # test features
+VAL_FEATS = '../jsons/val_features.pkl' # val features
+
+TRAIN_LABELS = '../jsons/train_labels.json' # train labels
+TEST_LABELS = '../jsons/test_labels.json' # test labels
+VAL_LABELS = '../jsons/val_labels.json' # val labels
